45e2d8a7bb35292dbaf7b68cf943cfc73b5319e6
Hiraku Onuma <hiraku00@gmail.com>
2023-12-19 15:10:28 +0400

    add a function to select and run a model(LLaMA/Gemini Pro/Gemini Pro Vision)

--- back/app.py
+++ back/app.py
@@ -1,42 +1,105 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from llama_cpp import Llama
+import google.generativeai as genai
+import PIL.Image
+import os
+from dotenv import load_dotenv
+import json
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/process', methods=['POST'])
-def process_text():
-    data = request.get_json()
-    messages = data['messages']
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-    # メッセージのリストを作成
-    formatted_messages = [
+MODEL_LLAMA_PATH = "./models/codellama-7b-instruct.Q4_K_S.gguf"
+MODEL_LLAMA = "llama2-7B"
+MODEL_GEMINI_PRO = "gemini-pro"
+MODEL_GEMINI_PRO_V = "gemini-pro-vision"
+
+config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 4096,
+}
+
+llama = Llama(
+    model_path=MODEL_LLAMA_PATH,
+    n_gpu_layers=1, # use GPU
+    verbose=False,  # omit log output
+)
+gem_p = genai.GenerativeModel(model_name=MODEL_GEMINI_PRO,
+                                generation_config=config)
+gem_p_v = genai.GenerativeModel(model_name=MODEL_GEMINI_PRO_V,
+                                generation_config=config)
+
+def proc_llama2():
+    messages = json.loads(request.form['messages'])
+    msg = [
         {
             "role": message['type'],
             "content": message['text'],
         }
         for message in messages
     ]
-
-    llm = Llama(
-        model_path="./models/codellama-7b-instruct.Q4_K_S.gguf",
-        n_gpu_layers=1, # use GPU
-        verbose=False,  # omit log output
-    )
-
-    # Generate Chat format
-    output = llm.create_chat_completion(
-        messages=formatted_messages,
+    response = llama.create_chat_completion(
+        messages=msg,
         temperature=0.1,
-        max_tokens=512,
+        max_tokens=4096,
     )
 
-    # レスポンスがアシスタントからのものであれば、それをチャットの履歴に追加
-    if output["choices"][0]["message"]["role"] == "assistant":
-        formatted_messages.append(output["choices"][0]["message"])
+    return jsonify({'text': response["choices"][0]["message"]["content"]})
 
-    return jsonify({'text': [message["content"] for message in formatted_messages if message["role"] == "assistant"]})
+def proc_gemini_pro():
+    messages = json.loads(request.form['messages'])
+    msg = [
+        {
+            "role": message['type'],
+            "parts": [message['text']],
+        }
+        for message in messages
+    ]
+    chat = gem_p.start_chat(history=msg)
+    text = request.form['text']
+    try:
+        response = chat.send_message(text)
+    except Exception as e:
+        return jsonify({'errror': str(e)}), 500
+    return jsonify({'text': response.text})
+
+def proc_gemini_pro_vision():
+    text = request.form['text']
+
+    image = request.files["image"]
+    if image is None:
+        return jsonify({'eroor': 'No image provided'}), 400
+    img = PIL.Image.open(image)
+
+    response = gem_p_v.generate_content([
+        text,
+        img
+    ], stream=True)
+
+    response.resolve()
+    return jsonify({'text': response.text})
+
+@app.route('/process', methods=['POST'])
+def process_text():
+    model = request.form['model']
+
+    proc_func = {
+        MODEL_LLAMA: proc_llama2,
+        MODEL_GEMINI_PRO: proc_gemini_pro,
+        MODEL_GEMINI_PRO_V: proc_gemini_pro_vision,
+    }
+
+    func = proc_func.get(model)
+
+    if func is None:
+        return jsonify({'error': 'Invalid model name'}), 400
+    return func()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,12 +5,15 @@ import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
+  const [model, setModel] = useState("llama2-7B");
+  const [image, setImage] = useState<File | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isDisabled, setIsDisabled] = useState(false);
   const [history, setHistory] = useState(
-    [] as { type: string; text: string }[]
+    [] as { type: string; text?: string; parts?: string[] }[]
   );
   const bottomRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
 
   const scrollToBottom = () => {
     setTimeout(() => {
@@ -22,18 +25,69 @@ function App() {
     setIsLoading(true);
     setIsDisabled(true);
     try {
-      const response = await axios.post("http://localhost:5001/process", {
-        messages: history.concat({ type: "user", text: text.trim() }),
-      });
+      if (model === "llama2-7B") {
+        const formData = new FormData();
+        formData.append('messages', JSON.stringify(history.concat({ type: "user", text: text.trim() })));
+        formData.append('model', model);
 
-      setHistory((prev) => [
-        ...prev,
-        { type: "user", text },
-        { type: "assistant", text: response.data.text.join(' ') },
-      ]);
-      //debug
-      console.log(history)
-      setText(""); // 入力フィールドをクリア
+        const response = await axios.post("http://localhost:5001/process", formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+
+        setHistory((prev) => [
+          ...prev,
+          { type: "user", text },
+          { type: "assistant", text: response.data.text },
+        ]);
+      } else if (model === "gemini-pro") {
+        const formData = new FormData();
+        formData.append('messages', JSON.stringify(history));
+        formData.append('text', text);
+        formData.append('model', model);
+
+        const response = await axios.post("http://localhost:5001/process", formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+
+        setHistory((prev) => [
+          ...prev,
+          { type: "user", text },
+          { type: "model", text: response.data.text },
+        ]);
+      }
+      // "gemini-pro-vision"
+      else {
+        if (!image) {
+          console.log("No image file selected");
+          return;
+        }
+        // FIXME
+        // https://hatehate-nazenaze.hatenablog.com/entry/2020/07/16/151828
+        // https://qiita.com/melonpass/items/ff7fbfbb7edad2e768e2
+        const formData = new FormData();
+        formData.append('messages', JSON.stringify(history.concat({ type: "user", text: text.trim() })));
+        formData.append('text', text);
+        formData.append('image', image);
+        formData.append('model', model);
+
+        console.log(image);
+        const response = await axios.post("http://localhost:5001/process", formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+
+        setHistory((prev) => [
+          ...prev,
+          { type: "user", text },
+          { type: "model", text: response.data.text },
+        ]);
+      }
+      setText("");
     } catch (error) {
       console.error(error);
     } finally {
@@ -49,66 +103,102 @@ function App() {
     }
   }, [isLoading]);
 
+  useEffect(() => {
+    if (model !== "gemini-pro-vision") {
+      setImage(null);
+    }
+  }, [model]);
+
   return (
-    <div className="flex flex-col items-center space-y-4 w-2/3 mx-auto h-screen">
-      <div className="w-full h-5"></div>
-      <div className="w-full overflow-auto h-3/4 bg-gray-100 relative">
-        {history.map((item, i) => (
-          <div
-            key={i}
-            className={`p-4 my-2 ${
-              item.type === "user"
-                ? "rounded-br-none self-end text-white"
-                : "rounded-bl-none self-start text-black"
-            }`}
-          >
+    <div className="flex w-full mx-auto h-screen px-4">
+      <div className="w-1/6 h-screen p-4 space-y-4 border-r border-gray-300 pt-5">
+        <select
+          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          id="model-select"
+          onChange={(e) => {
+            setModel(e.target.value);
+            setHistory([]);
+          }}
+        >
+          <option value="llama2-7B">LLama2 - 7B</option>
+          <option value="gemini-pro">Gemini Pro</option>
+          <option value="gemini-pro-vision">Gemini Pro Vision</option>
+        </select>
+        {model === "gemini-pro-vision" && (
+          <input
+            type="file"
+            onChange={(e) => e.target.files && setImage(e.target.files[0])}
+          />
+        )}
+        {image && (
+          <img
+            ref={imageRef}
+            src={URL.createObjectURL(image)}
+            alt="uploaded"
+            style={{ width: "200px", height: "200px" }}
+          />
+        )}
+      </div>
+      <div className="flex flex-col items-center space-y-4 w-5/6 mx-auto h-screen px-5">
+        <div className="w-full h-5"></div>
+        <div className="w-full overflow-auto h-4/5 bg-gray-100 relative">
+          {history.map((item, i) => (
             <div
-              className={`border border-gray-300 rounded-md p-2 ${
+              key={i}
+              className={`p-4 my-2 ${
                 item.type === "user"
-                  ? "w-auto inline-block float-right bg-green-500 max-w-[80%]"
-                  : "w-auto inline-block float-left bg-white max-w-[80%]"
+                  ? "rounded-br-none self-end text-white"
+                  : "rounded-bl-none self-start text-black"
               }`}
             >
-              {item.text}
+              <div
+                className={`border border-gray-300 rounded-md p-2 ${
+                  item.type === "user"
+                    ? "w-auto inline-block float-right bg-green-500 max-w-[80%]"
+                    : "w-auto inline-block float-left bg-white max-w-[80%]"
+                }`}
+              >
+                {item.text}
+              </div>
+              <div className="clear-both"></div>
             </div>
-            <div className="clear-both"></div>
-          </div>
-        ))}
-        <div ref={bottomRef}></div>
+          ))}
+          <div ref={bottomRef}></div>
+        </div>
+        <div className="w-full h-1/5 flex flex-col items-center justify-center space-y-4 relative">
+          <AnimatePresence>
+            {isLoading && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="apple-loader absolute bottom-0 left-1/2 transform -translate-x-1/2"
+              ></motion.div>
+            )}
+          </AnimatePresence>
+          <motion.textarea
+            id="userInput"
+            name="userInput"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 w-2/3 h-20 overflow-hidden"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            disabled={isDisabled}
+          />
+          <motion.button
+            onClick={processText}
+            disabled={isLoading}
+            className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            {isLoading ? "Searching..." : "Search"}
+          </motion.button>
+        </div>
+        <div className="w-full h-5"></div>
       </div>
-      <div className="w-full h-1/5 flex flex-col items-center justify-center space-y-4 relative">
-        <AnimatePresence>
-          {isLoading && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className="apple-loader absolute bottom-0 left-1/2 transform -translate-x-1/2"
-            ></motion.div>
-          )}
-        </AnimatePresence>
-        <motion.textarea
-          id="userInput"
-          name="userInput"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 w-2/3 h-20 overflow-hidden"
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
-          disabled={isDisabled}
-        />
-        <motion.button
-          onClick={processText}
-          disabled={isLoading}
-          className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-        >
-          {isLoading ? "Searching..." : "Search"}
-        </motion.button>
-      </div>
-      <div className="w-full h-5"></div>
     </div>
   );
 }
